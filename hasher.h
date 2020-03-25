adc5eec53cbf5c623857ad8a85880236ff70f4fe
Gabe Rivera <58158031+gabe-l-rivera@users.noreply.github.com>
2020-03-24 22:20:53 -0500

    Update hasher.h

--- hasher.h
+++ hasher.h
@@ -1,34 +1,29 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 #ifndef HASHER_H
 #define HASHER_H
 
 class hasher{
-    
+
 private:
-    static const int t_s = 363;
-    
-    struct item{
-        string word;
-        //int count; //int
-        item* next;
-    };
-    
-    item* HashTable[t_s];
+        static const int t_s = 363; //equal to number of words
+
+        struct item{
+                string word;
+                int count;
+                item* next;
+        };
+
+        item* HashTable[t_s];
 public:
-    hasher();
-    int Hash(string key);
-    void addItem(string name);
-    int numberOfItems(int index);
-    void printTable();
-    void createArrays(string arr1[], int arr2[], int d);
-    
+        hasher();
+        int Hash(string key);
+        void addItem(string name);
+        void sortVectors(vector<string> vect1, vector<int> vect2);
 };
-
-
 
 #endif //HASHER_H
